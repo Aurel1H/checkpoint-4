6935173a7738add719fb5ccfdd75b1083713ce84
Aurélien HAYE <aurelien.haye.techdeco@gmail.com>
2025-02-12 20:01:17 +0100

    feat: schema creation

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -1,21 +1,41 @@
-create table user (
-  id int unsigned primary key auto_increment not null,
-  email varchar(255) not null unique,
-  password varchar(255) not null
+-- SQLBook: Code
+CREATE TABLE IF NOT EXISTS user (
+id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+firstname VARCHAR(50) NOT NULL,
+lastname VARCHAR(50) NOT NULL,
+email VARCHAR(150) NOT NULL UNIQUE,
+hash_password VARCHAR(255) NOT NULL,
+avatar VARCHAR(255) NOT NULL,
+created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP);
+
+CREATE TABLE IF NOT EXISTS book (
+  id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  title VARCHAR(50) NOT NULL,
+  summary TEXT NOT NULL,
+  genre VARCHAR(50),
+  language VARCHAR(50),
+  author VARCHAR(100) NOT NULL,
+  publisher VARCHAR(100),
+  illustrator VARCHAR(100),
+  owner VARCHAR(100) NOT NULL,
+  isbn VARCHAR(15) NOT NULL UNIQUE,
+  page_count INT,
+  pickup_address VARCHAR(100) NOT NULL,
+  statut VARCHAR(100) NOT NULL,
+  cover_image VARCHAR(255),
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
-create table item (
-  id int unsigned primary key auto_increment not null,
-  title varchar(255) not null,
-  user_id int unsigned not null,
-  foreign key(user_id) references user(id)
+CREATE TABLE IF NOT EXISTS user_book (
+  id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  user_id INT NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES user(id),
+  book_id INT NOT NULL,
+  FOREIGN KEY (book_id) REFERENCES book(id)
 );
 
-insert into user(id, email, password)
-values
-  (1, "jdoe@mail.com", "123456");
 
-insert into item(id, title, user_id)
-values
-  (1, "Stuff", 1),
-  (2, "Doodads", 1);
